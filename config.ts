fd41bb8522f1a7424ae06582163af79532f21cbc
Ricardo Trejos <cardotrejos@gmail.com>
2022-06-26 12:52:52 -0500

    prod env

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `fill me in when we deploy`;
+export const prodEndpoint = `https://laspolas-api-production.up.railway.app/`;
 export const perPage = 20;
